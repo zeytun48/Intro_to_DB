4386d2f228be5c47b9476fdce7c293c711b8eb61
zeytun48 <dahiyezeytun56@gmail.com>
2024-10-20 10:47:35 -0700

    Update task_5.sql to insert new customer

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,6 @@
-INSERT INTO Customers (customer_id, customer_name, email, address) 
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT INTO Customers (customer_id, customer_name, email, address) VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+
 
 
 
