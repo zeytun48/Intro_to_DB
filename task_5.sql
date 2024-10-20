a05933d27b3d60889e3c51b9e2e28dda3837da5d
zeytun48 <dahiyezeytun56@gmail.com>
2024-10-20 09:49:17 -0700

    Insert customer data into Customers table

---
+++ task_5.sql
@@ -0,0 +1,7 @@
+DELETE FROM Customers WHERE customer_id = 1;
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+
+
