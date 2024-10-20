57c67f2f6c06310a47c40750da30ac4496e0f992
zeytun48 <dahiyezeytun56@gmail.com>
2024-10-20 10:12:28 -0700

    Add single row to Customers table

--- task_5.sql
+++ task_5.sql
@@ -1,7 +1,9 @@
-DELETE FROM Customers WHERE customer_id = 1;
-
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO Customers (customer_id, customer_name, email, address) 
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+
+
+
 
 
 
