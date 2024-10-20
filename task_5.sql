913fa1dfcb784bee9b15e1a1535cdbd8e66099b8
zeytun48 <dahiyezeytun56@gmail.com>
2024-10-20 10:23:28 -0700

    Fix: Update task_5.sql for correct insertion

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,7 @@
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO Customers (customer_id, customer_name, email, address) 
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+
 
 
 
