15bf9933a90aa1eef4d3fb4356d5f45c5990aec2
zeytun48 <dahiyezeytun56@gmail.com>
2024-10-20 01:19:01 -0700

    Update MySQLServer.py to include exception handling and avoid SELECT/SHOW statements

--- MySQLServer.py
+++ MySQLServer.py
@@ -9,17 +9,17 @@ def create_database():
             user='root',
             password='@zeytun29'  
         )
-        
+
         if connection.is_connected():
             # Create a cursor object using the connection
             cursor = connection.cursor()
             # Create the database
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
-    
+
     except Error as e:
         print(f"Error while connecting to MySQL: {e}")
-    
+
     finally:
         if connection.is_connected():
             cursor.close()
@@ -28,4 +28,34 @@ def create_database():
 
 if __name__ == "__main__":
     create_database()
+
+
+
+
+
+   
+  
+
+       
+ 
+        
+        
+        
+    
+            
+     
+       
+          
+    
+
+
+    
+
+
+  
+
+
+
+
+
 
