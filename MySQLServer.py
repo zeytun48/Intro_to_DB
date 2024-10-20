235dc680083562b497c7abfc6d6869739937a07d
zeytun48 <dahiyezeytun56@gmail.com>
2024-10-20 01:06:20 -0700

    Added exception handling to MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -2,48 +2,25 @@ import mysql.connector
 from mysql.connector import Error
 
 def create_database():
-<<<<<<< HEAD
-    # Database credentials
-    host_name = 'localhost'
-    user_name = 'root'
-    password = '@zeytun29'  
-
     try:
-        # Establishing the connection
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            password=password
-        )
-
-        # Check if the connection is successful
-        if connection.is_connected():
-            cursor = connection.cursor()
-            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
-            print("Database 'alx_book_store' created successfully!")
-    
-=======
-    try:
+        # Establish a connection to the MySQL server
         connection = mysql.connector.connect(
             host='localhost',
-            user='root',    
-            password='@zeytun29'     
+            user='root',
+            password='@zeytun29'  
         )
-
+        
         if connection.is_connected():
+            # Create a cursor object using the connection
             cursor = connection.cursor()
+            # Create the database
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
-
->>>>>>> f473d62f031780257ccd781f1cebd1ec930cd7b1
+    
     except Error as e:
         print(f"Error while connecting to MySQL: {e}")
     
     finally:
-<<<<<<< HEAD
-        # Closing the connection
-=======
->>>>>>> f473d62f031780257ccd781f1cebd1ec930cd7b1
         if connection.is_connected():
             cursor.close()
             connection.close()
@@ -52,7 +29,3 @@ def create_database():
 if __name__ == "__main__":
     create_database()
 
-<<<<<<< HEAD
-=======
-
->>>>>>> f473d62f031780257ccd781f1cebd1ec930cd7b1
