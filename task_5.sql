57c42dd8356ccd3e9dc8929ff0715734623a4fef
zeytun48 <dahiyezeytun56@gmail.com>
2024-10-24 23:22:38 -0700

    Add task_5.sql to insert customer data

--- task_5.sql
+++ task_5.sql
@@ -1,11 +1,49 @@
 -- Single data insertion
+
 -- customer_id = 1
 -- customer_name = Cole Baidoo
 -- email = cbaidoo@sandtech.com
 -- address = 123 Happiness Ave.
-INSERT INTO Customers (customer_id, customer_name, email, address) VALUES
-(1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
-ON DUPLICATE KEY UPDATE customer_name = 'Cole Baidoo', email = 'cbaidoo@sandtech.com', address = '123 Happiness Ave.';
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
+(1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
