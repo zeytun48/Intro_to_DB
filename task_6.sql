2731819c2d37e83ef411fb4be12440037a7ed5f3
zeytun48 <dahiyezeytun56@gmail.com>
2024-10-24 23:32:49 -0700

    Inserted multiple rows into the customer table

--- task_6.sql
+++ task_6.sql
@@ -1,8 +1,29 @@
--- Insert multiple customers into the Customers table
-INSERT INTO Customers (customer_id, customer_name, email, address) VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+-- task_6.sql
+
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES 
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.')
+ON DUPLICATE KEY UPDATE 
+customer_name = VALUES(customer_name),
+email = VALUES(email),
+address = VALUES(address);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
